aa142c6bb21f2e1a5c5bf5b896b823ab35bcfe0b
Nikhil Pandey <91266675+Nikhil70700@users.noreply.github.com>
2025-02-26 13:44:15 +0530

    Query Params In Express

--- Query Parameters/app.js
+++ Query Parameters/app.js
@@ -1,42 +1,15 @@
 import express from 'express';
-import path from 'path';
+
+const app = express();
 
 
-
-const app=express();
-
-
-app.get("/",(req,res)=>{
-    res.send("Hello Query");
-});
-
-app.get("/contact",(req,res)=>{
-    res.send("Contact us at 1234567890");
-});
-
-
-app.get("/product",(req,res)=>{
+app.get("/search", (req, res) => {
+    const { query, limit } = req.query;  // Extract query parameters
+    res.send(`Searching for: ${query}, Limit: ${limit}`);
     console.log(req.query)
-    res.send(`<h1>User search for product ${req.query.page} ${req.query.limit}</h1>`);
-})
-
-app.get('/profile/:name',(req,res)=>{
-    console.log(res.params);
-    res.send(`<h1>My Name is ${req.params.name}</h1>`)
 });
 
-app.get('/profile/:username/article/:slug',(req,res)=>{
-   
-    // for space we are getting %20 in url to fix it we can write it like using regex
 
-    const formattedName=req.params.username.replace(/-/g," ");
-
-    const formattedSlug=req.params.slug.replace(/-/g," ");
-
-    res.send(`<h1>My Name is ${formattedName} learning Article ${formattedSlug}</h1>`)
-})
-
-
-app.listen(3000,()=>{
-    console.log(`Server is running on port 3000`);
+app.listen(3000, () => {
+    console.log("✅ Server is running on port 3000");
 });
